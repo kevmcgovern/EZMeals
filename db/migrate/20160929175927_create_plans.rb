681653e2063df91423a25d022b94841be8e8fd39
Kevin M <kevin.mcgovern92@gmail.com>
2016-09-29 11:15:58 -0700

    Add all migrations

---
+++ db/migrate/20160929175927_create_plans.rb
@@ -0,0 +1,12 @@
+class CreatePlans < ActiveRecord::Migration[5.0]
+  def change
+    create_table :plans do |t|
+    	t.string :plan_name
+    	t.string :time_frame
+    	t.integer :calories
+    	t.text :recipe_collection
+
+    	t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20160929175935_create_ingredients.rb
@@ -0,0 +1,12 @@
+class CreateIngredients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :ingredients do |t|
+    	t.string :name
+    	t.string :amount
+    	t.string :unit
+    	t.integer :spoon_id
+
+    	t.timestamps null: false
+    end
+  end
+end
