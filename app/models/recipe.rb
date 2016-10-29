f0f407b3bd0d3ae4b556874708d9d33bb7462620
Kevin M <kevin.mcgovern92@gmail.com>
2016-10-28 17:22:57 -0700

    Updated Recipe model to destroy dependent upon deletion

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
 	belongs_to :plan
-	has_many :ingredients
+	has_many :ingredients, :dependent => :destroy
 	accepts_nested_attributes_for :ingredients
 	# has_many :ingredients, :through => :recipe_ingredients
 end
