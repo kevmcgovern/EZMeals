db30c484a1e243c3c9678bb9bded0f3f877668b4
Kevin M <kevin.mcgovern92@gmail.com>
2016-11-03 15:56:39 -0700

    Add instructions column to recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103214846) do
+ActiveRecord::Schema.define(version: 20161103224603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20161103214846) do
     t.integer  "plan_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.text     "instructions"
     t.index ["plan_id"], name: "index_recipes_on_plan_id", using: :btree
   end
 
