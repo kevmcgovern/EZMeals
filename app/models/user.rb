ae6b525e9052cd4b423a9cddb0fe3bcc292a121f
Kevin M <kevin.mcgovern92@gmail.com>
2016-11-17 14:13:07 -0800

    Add validation for password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
 
 	validates :name, presence: true, length: { minimum: 5 }
 	validates :email, presence: true, length: { minimum: 4 }
+	validates :password, presence: true
 end
